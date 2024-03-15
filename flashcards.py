6eb69cbe05be3333de3f9093fe4fa412090f07f9
Thien Ho <hqt1@usf.edu>
2024-03-14 22:23:49 -0400

    initial commit

---
+++ flashcards.py
@@ -0,0 +1,10 @@
+card_input = input()
+
+definition_input = input()
+
+answer = input()
+
+if answer == definition_input:
+    print("Your answer is right!")
+else:
+    print("Your answer is wrong...")
